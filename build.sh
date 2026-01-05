ddafc0fd4d337579cee7022c0f2421574360078d
tazo7mod-dotcom <tazo.7mdoi@gmail.com>
2026-01-05 12:06:17 +0300

    as

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
-#!/usr/bin/env bash
-# Exit on error
+
 set -o errexit
 
-echo "Installing dependencies..."
+
 pip install -r requirements.txt
 
-echo "Creating persistent directory..."
 
-mkdir -p /var/data
+python -c "from app import create_app, db; app = create_app(); app.app_context().push(); db.create_all()"
 
-echo "Applying Database Migrations..."
 
+flask db stamp head
+flask db migrate 2>/dev/null || true 
 flask db upgrade
+
+
+python setup_data.py
